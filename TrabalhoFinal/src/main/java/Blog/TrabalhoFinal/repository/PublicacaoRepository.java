e72b7303e5ade9ae357c1763032a94e665d51535
ariellybispo <aribispo030@gmail.com>
2025-11-27 16:20:25 -0300

    editar e excluir

--- TrabalhoFinal/src/main/java/Blog/TrabalhoFinal/repository/PublicacaoRepository.java
+++ TrabalhoFinal/src/main/java/Blog/TrabalhoFinal/repository/PublicacaoRepository.java
@@ -1,5 +1,6 @@
 package Blog.TrabalhoFinal.repository;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,9 +14,14 @@ public class PublicacaoRepository {
     private List<Publicacao> lista = new ArrayList<>();
     private int contador = 1;
 
-  public PublicacaoRepository() {
-    
-}
+    public PublicacaoRepository() {
+        lista.add(new Publicacao(contador++, "Alunos talentosos", "Adriana Oliveira ",
+                LocalDate.of(2025, 10, 31), "Ser um bom estudante universitário é menos sobre um talento inato e mais sobre cultivar uma mentalidade e um ritmo de vida propícios à absorção do saber. Não se trata de acumular notas altas por si só, mas de construir uma fundação sólida de compreensão crítica e autodisciplina."));
+
+        lista.add(new Publicacao(contador++, "Como ser rico?", "Neymar Jr.",
+                LocalDate.of(2025, 12, 2), "Apenas nasça com talento."));
+    }
+
 
 
     public List<Publicacao> listar() { return lista; }
